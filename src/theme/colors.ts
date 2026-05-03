3018488b6c71466127465f6f0a02fb8f8259f613
mateusrossi-jpg <mateusrossi@icloud.com>
2026-05-03 11:09:36 -0300

    Add color theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  background: '#071114',
+  surface: '#0E1B20',
+  surfaceElevated: '#14262D',
+  border: '#24424C',
+  text: '#E8F3F5',
+  textMuted: '#8EA7AD',
+  cyan: '#20D3E6',
+  cyanSoft: '#123D45',
+  green: '#3EE58B',
+  amber: '#F5B942',
+  red: '#FF5A67',
+  inactive: '#4E666D',
+};
