26e49ed3c8e25d53eac69d4d50695b7896e536d0
mateusrossi-jpg <mateusrossi@icloud.com>
2026-05-05 06:53:48 -0300

    refactor: use workspace tabs in projects screen

--- src/components/premium/PremiumProjectsScreen.tsx
+++ src/components/premium/PremiumProjectsScreen.tsx
@@ -14,8 +14,7 @@ import { searchTrainingProjects } from '../../projects/projectSearch';
 import { colors } from '../../theme/colors';
 import { spacing } from '../../theme/spacing';
 import { PremiumBadge, PremiumMetric, PremiumScreen, PremiumSection, PremiumSegmented } from './index';
-import { PremiumProjectDetailCard } from './PremiumProjectDetailCard';
-import { PremiumProjectExportCodePreviews } from './PremiumProjectExportCodePreviews';
+import { PremiumProjectWorkspace } from './PremiumProjectWorkspace';
 
 type ProjectFilter = ProjectCatalogFilter;
 
@@ -69,8 +68,7 @@ export const PremiumProjectsScreen = memo(function PremiumProjectsScreen() {
         <PremiumMetric label="Filtrados" value={filteredProjects.length} hint="resultado" tone="green" />
       </View>
 
-      <PremiumProjectDetailCard project={selectedProject} />
-      <PremiumProjectExportCodePreviews project={selectedProject} />
+      <PremiumProjectWorkspace project={selectedProject} />
 
       <PremiumSection title="Projeto em destaque" subtitle="Ideal para iniciar no pensamento Ladder" tone="green">
         <View style={styles.featuredCard}>
